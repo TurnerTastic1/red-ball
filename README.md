5ea6a7a0129d4f0f4dddf0073bfa3d2977c1673a
Turner Naef <Turner.Naef@gmail.com>
2022-07-08 16:51:36 -0400

    The Red Ball is now an item with basic properties

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
-# Fabric Example Mod
+# Red Ball Mod
+Adds a red ball to minecraft that has basic item properties
 
 ## Setup
+Clone the repo then go to debug GUI then run Minecraft Client
 
-For setup instructions please see the [fabric wiki page](https://fabricmc.net/wiki/tutorial:setup) that relates to the IDE that you are using.
 
 ## License
 
 This template is available under the CC0 license. Feel free to learn from it and incorporate it in your own projects.
+
+
+## Credit
+
+https://favpng.com/png_view/rood-minecraft-video-game-pixel-art-super-smash-bros-png/MGB54BXT - the Red_Ball
--- src/main/java/turnertastic/red/ball/RedBall.java
+++ src/main/java/turnertastic/red/ball/RedBall.java
@@ -1,21 +1,28 @@
-package net.fabricmc.example;
+package turnertastic.red.ball;
 
 import net.fabricmc.api.ModInitializer;
+
+import net.minecraft.util.Identifier;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.registry.Registry;
 
-public class ExampleMod implements ModInitializer {
+public class RedBall implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final Logger LOGGER = LoggerFactory.getLogger("redball");
+
+	public static final Item RED_BALL = new Item(new Item.Settings().group(ItemGroup.MISC));
 
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		Registry.register(Registry.ITEM, new Identifier("redball", "red_ball"), RED_BALL);
 	}
 }
